1b3013e6dddbcdf13720dc24326c1a2c7423b7e0
clk1006 <clk1006@web.de>
2019-01-25 15:46:51 +0100

    finish first versiuon

---
+++ pages/index.js
@@ -0,0 +1,71 @@
+import { useState, useEffect } from 'react';
+export default () => {
+	const [ started, setStarted ] = useState(false);
+	const [ clicks, setClicks ] = useState(0);
+	const [ ended, setEnded ] = useState(false);
+	useEffect(
+		() => {
+			if (started) {
+				setClicks(0);
+				setTimeout(() => {
+					setStarted(false);
+					setEnded(true);
+				}, 5000);
+			}
+		},
+		[ started ]
+	);
+
+	return (
+		<div className="root">
+			<h1>The click detector</h1>
+			<button
+				onClick={(event) => {
+					if (!started && !ended) {
+						setStarted(true);
+					} else if (started) {
+						setClicks(clicks + 1);
+					}
+				}}
+			>
+				{!started && !ended && 'Click me to start and play the game for five seconds'}
+				{started && 'Click me!!!'}
+				{ended && 'Reload to play again'}
+			</button>
+			{ended && (
+				<h2>
+					Clicks: {clicks}
+					<br />Clicks per Second: {clicks / 5}
+				</h2>
+			)}
+			<style jsx>{`
+				:global(body) {
+					background: lightgrey;
+				}
+				h1 {
+					text-align: center;
+					color: darkblue;
+					font-size: 5vw;
+				}
+				.root {
+					display: flex;
+					flex-direction: column;
+					justify-content: center;
+				}
+				h2 {
+					text-align: center;
+					color: darkpink;
+					font-size: 4vw;
+				}
+				button {
+					background: lightgreen;
+					color: red;
+					font-size: 400%;
+					width: 50vw;
+					margin-left: 25vw;
+					height: 30vh;
+				}
+			`}</style>
+		</div>
+	);
+};
